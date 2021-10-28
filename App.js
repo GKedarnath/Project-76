bfa2c776de0b58ec5d8e153a2d5c0b7d9cd08484
GKedarnath <72261336+GKedarnath@users.noreply.github.com>
2021-10-28 11:16:35 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native';
-//import { StackNavigator } from '@react-navigation/stack';
-import { createStackNavigator } from '@react-navigation/stack';
-//import { createStackNavigation } from '@react-navigation/stack';
-//import { createStack } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack'
+import HomeScreen from './Screens/HomeScreens'
+import ISSLocationScreen from './Screens/ISSLocationScreens';
+import MeteorScreen from './Screens/MeteorScreens';
 
-import HomeScreen from "./screens/Home";
-import StartMapScreen from "./screens/StarMap";
-import DailyPicScreen from "./screens/DailyPic";
-import SpaceCraftScreen from "./screens/SpaceCraft";
+const Stack = createStackNavigator()
 
-const Stack = createStackNavigator();
-
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{
-        headerShown: false
-      }}>
-    
-        {/* <Stack.Screen component="Home" name={HomeScreen} /> */}
-            <Stack.Screen name="Home" component={HomeScreen} /> 
-        {/* <Stack.Screen name="Home" component="HomeScreen" /> */}
-        {/* <Stack.Screen name:"Home" component:{HomeScreen} /> */}
-    
-        <Stack.Screen name="StarMap" component={StartMapScreen} />
-        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
-        <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+     <NavigationContainer>
+       <Stack.Navigator initialRouteName = "Home" screenOptions = {{headerShown: false}}>
+         <Stack.Screen name = "Home" component = {HomeScreen}/>
+         <Stack.Screen name = "ISSLocation" component = {ISSLocationScreen}/>
+         <Stack.Screen name = "Meteor" component = {MeteorScreen}/>
+          
+       </Stack.Navigator>
+     </NavigationContainer>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
